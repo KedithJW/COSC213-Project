0a7df0dd7cb8d0cdaca6716678636fbbb7c7b2c6
Jordan Kober <153781917+jordankober@users.noreply.github.com>
2025-12-01 12:07:48 -0800

    Update board.js
    Removed complete label. Didn't fit in the task card with the inclusion of photo button

--- public/board.js
+++ public/board.js
@@ -168,7 +168,7 @@ $(document).ready(function() {
     const newTask = $("<li>").addClass("list-group-item task new-task").attr('draggable', 'true');
 
     // Append it to the list
-    taskTopRow.append(newDeleteBtn, completeBtn, photoBtn, taskName);
+    taskTopRow.append(taskName, newDeleteBtn, completeBtn, photoBtn);
     newTask.append(taskTopRow);
     newTask.append(taskDescription);
     listGroup.prepend(newTask);
@@ -419,13 +419,13 @@ $(document).ready(function() {
     //////////////////////////////
     if (status[0] == 1) {
       const completeBtn = $('<i>').addClass('btn btn-primary complete-task-btn bi bi-check-circle').css('pointer-events', 'none');
-      const completeLabel = $('<span>').addClass('ms-2 complete-label').text('(complete)');
-      taskTopRow.append(newDeleteBtn, completeBtn, photoBtn, taskName, completeLabel);
+      //const completeLabel = $('<span>').addClass('ms-2 complete-label').text('(complete)');
+      taskTopRow.append(taskName, newDeleteBtn, completeBtn, photoBtn,);
       newTask.append(taskTopRow, taskDescription); // ENDED HERE TO GO IMPLEMENT DESCRIPTION BACKEND
       listGroup.append(newTask);                   // finish for other status and it might work...
     } else {
         const completeBtn = $('<i>').addClass("btn btn-light complete-task-btn bi bi-check-circle");
-        taskTopRow.append(newDeleteBtn, completeBtn, photoBtn, taskName);
+        taskTopRow.append(taskName, newDeleteBtn, completeBtn, photoBtn);
         newTask.append(taskTopRow, taskDescription);
         listGroup.append(newTask);
     }
@@ -454,7 +454,7 @@ $(document).ready(function() {
     if(completeBtn) {
       const btn = $(completeBtn)
       btn.removeClass('btn-light').addClass('btn-primary').css('pointer-events', 'none');
-      btn.next().after('<span class="ms-2 complete-label">(complete)</span>'); //This adds before task name... FIX!!
+      //btn.next().after('<span class="ms-2 complete-label">(complete)</span>'); //This adds before task name... FIX!!
       const li = btn.closest('.existing-task');
       const ul = li.parent();
       ul.append(li);
